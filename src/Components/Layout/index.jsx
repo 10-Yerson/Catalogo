417ddca2160bd6a753d804e0648b93b6d6f21861
miltonyela <miltonyela@gmail.com>
2023-10-20 17:52:46 -0500

    Actualización y consumo API

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,8 +1,20 @@
-const Layout = ({ children }) => {
+
+
+/* const Layout = ({ children }) => {
     return (
         <div className="flex flex-col items-center mt-20">
             {children}
         </div>
     )
+} */
+
+
+const Layout = ({ children }) => {
+    return (
+        <div className="flex flex-col items-center mt-20 bg-slate-200">
+            {children}
+        </div>
+    )
 }
+
 export default Layout
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from 'react'
+import React from 'react'
+
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
+function Home() {
+
+    const [items, setItems] = useState(null)
+    useEffect(() => {
+        // https://fakeapi.platzi.com/
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+
+    }, [])
+    return (
+        <>
+            <Layout>
+                Home
+                <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                    {
+                        items?.map(item => (
+                            <Card key={item.id} data={item} />
+                        ))
+                    }
+
+                </div>
+            </Layout>
+        </>
+
+    )
+}
+export default Home
+
+/*
 function Home() {
     return (
         <>
@@ -12,4 +45,7 @@ function Home() {
     )
 }
 
-export default Home
+export default Home  
+
+*/
+
